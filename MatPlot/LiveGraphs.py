48eb51686aff0f9c4fcb0573bfc337b030f7171f
sahilk335 <sahilk335@gmail.com>
2018-08-12 22:03:06 +0530

    LiveGraphs

---
+++ MatPlot/LiveGraphs.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 21:58:46 2018
+
+@author: sahil
+
+Title : Live Graphs
+
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+
+def animate(i):
+    graph_data = open('sample.csv','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    ax1.clear()
+    ax1.plot(xs, ys)
+    
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
